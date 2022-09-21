d3e319de3c3cad6b0715f760a3530bdf0701d3f7
esh8904 <esh8904@gmail.com>
2022-09-21 15:09:30 +0900

    1509

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
